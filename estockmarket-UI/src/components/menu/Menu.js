6f77f98da6035a56db00db21e4b3f89c046b50f8
Suresh <sureshkasinathan96@gmail.com>
2022-06-24 18:09:57 +0530

    Added logo

--- estockmarket-UI/src/components/menu/Menu.js
+++ estockmarket-UI/src/components/menu/Menu.js
@@ -2,13 +2,17 @@ import React from "react";
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Cognizant_logo from '../../assets/cognizant.jpg';
 
 const Menu = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="/">Cognizant</Navbar.Brand>
+                
+                <Navbar.Brand href="/">
+                <img src={Cognizant_logo} alt="logo" />
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
