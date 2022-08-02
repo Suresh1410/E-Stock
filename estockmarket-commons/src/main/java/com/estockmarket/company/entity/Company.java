1abab6f0e6dfeea7ab933499a290e4f1c2417126
Suresh <sureshkasinathan96@gmail.com>
2022-08-02 12:56:43 +0530

    Register changes

--- estockmarket-commons/src/main/java/com/estockmarket/company/entity/Company.java
+++ estockmarket-commons/src/main/java/com/estockmarket/company/entity/Company.java
@@ -7,10 +7,18 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name="COMPANY")
 public class Company {
 	
@@ -28,4 +36,42 @@ public class Company {
 	private String companyWebsite;
 	@Column(name="STOCK_EXCHANGE")
 	private String stockExchange;
+	public Integer getCompanyCode() {
+		return companyCode;
+	}
+	public void setCompanyCode(Integer companyCode) {
+		this.companyCode = companyCode;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getCompanyCeo() {
+		return companyCeo;
+	}
+	public void setCompanyCeo(String companyCeo) {
+		this.companyCeo = companyCeo;
+	}
+	public String getCompanyTurnover() {
+		return companyTurnover;
+	}
+	public void setCompanyTurnover(String companyTurnover) {
+		this.companyTurnover = companyTurnover;
+	}
+	public String getCompanyWebsite() {
+		return companyWebsite;
+	}
+	public void setCompanyWebsite(String companyWebsite) {
+		this.companyWebsite = companyWebsite;
+	}
+	public String getStockExchange() {
+		return stockExchange;
+	}
+	public void setStockExchange(String stockExchange) {
+		this.stockExchange = stockExchange;
+	}
+	
+	
 }
